f2244af8876a173fd85b5c60382506427a9b8809
devarshipandey <devarshipandey83@gmail.com>
2025-03-17 19:20:25 +0530

    Updating settings.py

--- splitwise_rest_api/settings.py
+++ splitwise_rest_api/settings.py
@@ -124,11 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'splitwise', 'static')  # Your static folder path
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'css', 'static'),]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')  # Vercel expects this folder
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
